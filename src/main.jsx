90d25d5a710c313bbc197f592f3e4cfb7762dd9e
AngryDev <amelec2@gmail.com>
2024-01-14 12:30:32 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { EmpleadoCard } from './componentes/EmpleadoCard.jsx'
 import { CardInfo } from './componentes/CardInfo.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>  
